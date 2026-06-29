e64c5298d3b168c11c523a389731827234ff29fc
bloggerstar34-web <bloggerstar34@gmail.com>
2026-06-29 11:50:23 +0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function searchTools(){
+
+let input =
+document
+.getElementById("search")
+.value
+.toLowerCase();
+
+let cards =
+document
+.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let text =
+card.innerText.toLowerCase();
+
+if(text.includes(input))
+{
+card.style.display="block";
+}
+else
+{
+card.style.display="none";
+}
+
+});
+
+}
