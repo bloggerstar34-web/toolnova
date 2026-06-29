0b88d1a27840d59bed9eba16231652a21f2ab8cf
bloggerstar34-web <bloggerstar34@gmail.com>
2026-06-29 12:17:19 +0500

    Create tools.js

---
+++ js/js/tools.js
@@ -0,0 +1,15 @@
+function searchTools(){
+
+let input=document.getElementById("search").value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let text=card.innerText.toLowerCase();
+
+card.style.display=text.includes(input)?"block":"none";
+
+});
+
+}
